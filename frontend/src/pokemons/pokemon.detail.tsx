933ffe17ac15fb2a7153e743bfbcf8a9ce6351a2
Sander <sandervanhullebusch@gmail.com>
2023-06-21 00:42:41 +0200

    Update baseElement component

--- frontend/src/pokemons/pokemon.detail.tsx
+++ frontend/src/pokemons/pokemon.detail.tsx
@@ -1,12 +1,13 @@
-import { Alert, Tag } from "antd";
+import { Alert, Card, Tag, Progress } from "antd";
 import { FC, useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
 import * as pokemonService from "../services/pokemon.service"
 
 import { Page } from "../components/page";
-import { Pokemon } from "../types/pokemon";
+import { Pokemon, PokemonBase } from "../types/pokemon";
 import { typeColors } from "../misc/typeColors";
+import styles from './pokemon.detail.module.css';
 
 
 type RouteProps = {
@@ -16,6 +17,7 @@ type RouteProps = {
 export const PokemonDetail: FC = () => {
   const { id } = useParams<RouteProps>();
   const [pokemon, setPokemon] = useState<Pokemon | null>(null);
+  const [pokemonMaxBase, setPokemonMaxBase] = useState<PokemonBase | null>(null);
   const [error, setError] = useState<string | null>(null)
 
   useEffect(() =>  {
@@ -23,6 +25,14 @@ export const PokemonDetail: FC = () => {
     pokemonService.getPokemon(Number(id))
       .then(value => {
         setPokemon(value)
+      })
+      .catch((error: Error) => {
+        setError(error.message)
+      })
+
+    pokemonService.getMaxPokemonBase()
+      .then(value => {
+        setPokemonMaxBase(value)
       })
       .catch((error: Error) => {
         setError(error.message)
@@ -39,27 +49,64 @@ export const PokemonDetail: FC = () => {
   }
 
   // Catch empty pokemons state
-  if( !pokemon ) {
+  if( !pokemon || !pokemonMaxBase ) {
     return (
       <Page>
         <h1>No pokemon with id '{id}' found.</h1>
       </Page>
     );
   }
+
+  const baseElementComponent = (baseElement: string) => {
+    let percent = (pokemon.base[baseElement as keyof PokemonBase] / pokemonMaxBase[baseElement as keyof PokemonBase]) * 100
+    if(percent > 100) {
+      percent = 100
+    }
+
+    return (
+      <div key={baseElement} className={styles['base-element']}>
+        <p>{baseElement}</p>
+        <div className={styles['base-element__progress']}>
+          <Progress showInfo={false} percent={percent} />
+          <p>{pokemon.base[baseElement as keyof PokemonBase]}</p>
+        </div>
+      </div>
+    )
+  }
   
   return (
     <Page>
-      <h1>#{pokemon.id} {pokemon.name}</h1>
-      <div>
-        {pokemon.type.map(type => {
+      <Card loading={false}>
+        <h1>#{pokemon.id} {pokemon.name}</h1>
+        <div>
+          {pokemon.type.map(type => {
             return (
               <Tag color={typeColors[type.toLowerCase()]} key={type}>
-                {type}
-              </Tag>
-            );
-          })}
-      </div>
-      <p>{JSON.stringify(pokemon)}</p>
+                  {type}
+                </Tag>
+              );
+            })}
+        </div>
+        <hr style={{ marginTop: '1.25em'}} />
+
+        <div className={styles.base}>
+          <div>
+            {['HP', 'Speed'].map((baseElement: string) => {
+              return baseElementComponent(baseElement)
+            })}
+          </div>
+          <div>
+            {['Attack', 'Defense'].map((baseElement: string) => {
+              return baseElementComponent(baseElement)
+            })}
+          </div>
+          <div>
+            {['Sp. Attack', 'Sp. Defense'].map((baseElement: string) => {
+              return baseElementComponent(baseElement)
+            })}
+          </div>
+        </div>
+      </Card>
     </Page>
   );
 };
