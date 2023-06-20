e3d98c8bd69419150ff260b02dbb9a68e49a0f0e
Sander <sandervanhullebusch@gmail.com>
2023-06-20 21:28:56 +0200

    Use container in page component

--- frontend/src/components/page.tsx
+++ frontend/src/components/page.tsx
@@ -3,6 +3,7 @@ import { Content, Header } from "antd/es/layout/layout";
 import React, { FC, ReactNode } from "react";
 import { Link } from "react-router-dom";
 import logo from "../logo.png";
+import { Container } from "./container/container";
 
 interface Props {
     children: ReactNode;
@@ -12,11 +13,17 @@ export const Page: FC<Props> = ({ children }) => {
     return (
         <Layout>
             <Header>
-                <Link to={"/"}>
-                    <img src={logo} style={{ width: '150px' }} className="App-logo" alt="logo" />
-                </Link>
+                <Container>
+                    <Link to={"/"}>
+                        <img src={logo} style={{ width: '150px' }} className="App-logo" alt="logo" />
+                    </Link>
+                </Container>
             </Header>
-            <Content style={{ height: "100%" }}>{children}</Content>
+            <Content style={{ height: "100%", padding: "2rem 0" }}>
+                <Container>
+                    {children}
+                </Container>
+            </Content>
         </Layout>
     );
 };
