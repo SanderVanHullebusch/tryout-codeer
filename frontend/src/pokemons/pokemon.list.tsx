3cd3cef1b6946025c8b3163a35aac4c23d2400e9
Sander <sandervanhullebusch@gmail.com>
2023-06-20 21:35:51 +0200

    Show fetched pokemon on page

--- frontend/src/pokemons/pokemon.list.tsx
+++ frontend/src/pokemons/pokemon.list.tsx
@@ -1,12 +1,103 @@
-import React, { FC } from "react";
+import React, { FC, useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+
+import * as pokemonService from "../services/pokemon.service"
+import { Pokemon, PokemonType } from "../types/pokemon";
+
 import { Page } from "../components/page";
+import { Alert, Table, Tag } from "antd";
+import { typeColors } from "../misc/typeColors";
 
 export const PokemonList: FC = () => {
-  // Add here a table with the data from the server
+  const [pokemons, setPokemons] = useState<Pokemon[] | null>(null)
+  const [loading, setLoading] = useState<boolean>(false)
+  const [error, setError] = useState<string | null>(null)
+
+  useEffect(() => {
+    setLoading(true)
+
+    pokemonService.getPokemons()
+      .then(value => {
+        setLoading(false)
+        setPokemons(value)
+      })
+      .catch((error: Error) => {
+        setLoading(false)
+        setError(error.message)
+      })
+  }, []);
+
+  console.log('error', error)
+
+  // Catch error state
+  if( error ) {
+    return (
+      <Page>
+        <Alert message={error} type="error" showIcon />
+      </Page>
+    );
+  }
+
+  // Catch empty pokemons state
+  if( !pokemons ) {
+    return (
+      <Page>
+        <h1>No pokemons.</h1>
+      </Page>
+    );
+  }
+
+  const columns = [
+    {
+      title: 'Id',
+      dataIndex: 'id',
+      key: 'id',
+      width: '5em' // Give ID enough space to contain 3 characters
+    },
+    {
+      title: 'Name',
+      dataIndex: 'name',
+      key: 'name',
+    },
+    {
+      title: 'Type',
+      dataIndex: 'type',
+      key: 'type',
+      width: '15em', // Give types enough (but not too much) space
+      render: (types: PokemonType[]) => (
+        <>
+          {types.map(type => {
+            return (
+              <Tag color={typeColors[type.toLowerCase()]} key={type}>
+                {type}
+              </Tag>
+            );
+          })}
+        </>
+      ),
+    },
+  ];
+  
   return (
     <Page>
-      <Link to="/example">example</Link>
+      <Table 
+        dataSource={pokemons} 
+        columns={columns} 
+        rowKey="id" 
+        loading={loading}
+
+        onRow={(record, rowIndex) => {
+          return {
+            onClick: (event) => {}, // click row
+          };
+        }}
+
+        onHeaderRow={(columns, index) => {
+          return {
+            onClick: () => {}, // click header row
+          };
+        }}
+      />
     </Page>
   );
 };
