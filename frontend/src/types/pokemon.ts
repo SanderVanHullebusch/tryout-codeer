8c31acd3922a86ed21f47904f47e0c036e7280b2
Sander <sandervanhullebusch@gmail.com>
2023-06-20 20:33:12 +0200

    Add types for type, base & Pokemon itself

---
+++ frontend/src/types/pokemon.ts
@@ -0,0 +1,36 @@
+export enum PokemonType {
+    BUG = "bug",
+    DARK = "dark",
+    DRAGON = "dragon",
+    ELECTRIC = "electric",
+    FAIRY = "fairy",
+    FIRE = "fire",
+    FIGHTING = "fighting",
+    FLYING = "flying",
+    GHOST = "ghost",
+    GRASS = "grass",
+    GROUND = "ground",
+    ICE = "ice",
+    NORMAL = "normal",
+    POISON = "poison",
+    PSYCHIC = "psychic",
+    ROCK = "rock",
+    STEEL = "steel",
+    WATER = "water",
+}
+
+type PokemonBase = {
+    "HP": string,
+    "Attack": string,
+    "Defense": string,
+    "Sp. Attack": string,
+    "Sp. Defense": string,
+    "Speed": string
+}
+
+export type Pokemon = {
+    "id": number,
+    "name": string,
+    "type": PokemonType[],
+    "base": PokemonBase
+}
