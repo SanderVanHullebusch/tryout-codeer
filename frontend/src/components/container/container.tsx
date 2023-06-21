da15d6c0edbf7f016b36cb5d8abb97a6c3557ec6
Sander <sandervanhullebusch@gmail.com>
2023-06-21 17:05:28 +0200

    Let container extend props of html div element

--- frontend/src/components/container/container.tsx
+++ frontend/src/components/container/container.tsx
@@ -2,13 +2,13 @@ import { FC, ReactNode } from "react";
 
 import styles from './container.module.css'
 
-interface Props {
+interface Props extends Omit<React.HTMLProps<HTMLDivElement>, 'className'> {
     children: ReactNode;
 }
 
-export const Container: FC<Props> = ({ children }) => {
+export const Container: FC<Props> = ({ children, ...rest }) => {
     return (
-        <div className={styles.container}>
+        <div className={styles.container} {...rest}>
             {children}
         </div>
     );
